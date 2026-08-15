1d40d109e093b7e107caf57631e5e86a8636f135
Your Name <your@email.com>
2026-08-15 15:40:18 +0300

    getComputerChoice is added and tested

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(helloWorld!);
+function getComputerChoice() {
+  let n = Math.random() * 3
+  let i = Math.floor(n)
+  if (i === 0) {
+    return("Rock");
+  } else if (i === 1) {
+    return("Paper");
+  } else if (i === 2) {
+    return("Scissors")
+  }
+    }
+
+console.log(getComputerChoice())
